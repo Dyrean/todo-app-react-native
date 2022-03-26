f17c68bcfc2ad923449a7a7cf68c0bff6b45deb8
Fatih Canbey <fcanbeyy@gmail.com>
2022-03-27 02:39:04 +0300

    text input for task and add to array state and display it with flatlist

--- App.tsx
+++ App.tsx
@@ -1,20 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  NativeSyntheticEvent,
+  NativeTouchEvent,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+  const [inputTask, setInputTask] = useState("");
+  const [taskList, setTaskList] = useState<string[]>([]);
+
+  function handleTextInputChange(text: string) {
+    setInputTask(text);
+  }
+
+  function handleAddButton(event: NativeSyntheticEvent<NativeTouchEvent>) {
+    event.preventDefault();
+    setTaskList((currentTask: string[]) => [inputTask, ...currentTask]);
+  }
+
+  function handleTaskPress(removedTaskIndex: number) {
+    const array = [...taskList];
+    array.splice(removedTaskIndex, 1);
+    setTaskList([...array]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
+      <View style={styles.appContainer}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            value={inputTask}
+            onChangeText={handleTextInputChange}
+            placeholder="Enter Task To Do!"
+          />
+          <Button title="Add" onPress={handleAddButton} />
+        </View>
+        <View style={styles.tasksContainer}>
+          <FlatList
+            alwaysBounceVertical={false}
+            data={taskList}
+            renderItem={(itemData) => {
+              const { item, index } = itemData;
+              return (
+                <TouchableOpacity onPress={() => handleTaskPress(index)}>
+                  <View style={styles.taskItem}>
+                    <Text style={styles.taskText}>{item}</Text>
+                  </View>
+                </TouchableOpacity>
+              );
+            }}
+            keyExtractor={(_, index) => index.toString()}
+          />
+        </View>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 30,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
+  },
+  tasksContainer: {
+    flex: 4,
+  },
+  taskItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 12,
+    backgroundColor: "purple",
+  },
+  taskText: {
+    color: "white",
   },
 });
